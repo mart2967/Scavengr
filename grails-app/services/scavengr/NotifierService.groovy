4ec3c8596a37d64390ead971547922221a50eec1
Max Marti <mart2967@morris.umn.edu>
2013-05-02 20:13:17 +0000

    stringz
    git-svn-id: https://cscirepo.morris.umn.edu/svn/csci-3601/Spring13/Scavengr/IterationFour/MagmaLick@13025 e5a9bd0e-96e4-4e5d-b9da-31dc9101860d

--- grails-app/services/scavengr/NotifierService.groovy
+++ grails-app/services/scavengr/NotifierService.groovy
@@ -22,9 +22,9 @@ class NotifierService {
     def sendPasswordReset(email, newPassword) {
         mailService.sendMail {
             to email
-            from "Scavengr@scavengr.com"
-            subject "Scavengr Password Reset"
-            body "Your new password is ${newPassword}"
+            from 'Scavengr@scavengr.com'
+            subject 'Scavengr Password Reset'
+            body 'Your new password is ${newPassword}'
         }
     }
     
@@ -32,10 +32,10 @@ class NotifierService {
         link = grailsLinkGenerator.link(absolute: true, controller: 'hunt', action: 'show', params: [key: key])
 	mailService.sendMail {
 	    to email
-	    from "Scavengr@scavengr.com"
-	    subject "You have created the hunt ${title}"
-	    body "Hello ${name}, you have created a scavenger hunt! \
-            You can share this link with others to invite them to your hunt: ${link}"
+	    from 'Scavengr@scavengr.com'
+	    subject 'You have created the hunt ${title}'
+	    body 'Hello ${name}, you have created a scavenger hunt! \
+            You can share this link with others to invite them to your hunt: ${link}'
 	}
     }
 
@@ -45,17 +45,17 @@ class NotifierService {
 	mailService.sendMail {
 	    to emails
 	    from 'Scavengr@scavengr.com'
-	    subject "${myCreator} has invited you to their hunt, ${title}"
-	    body "Hello, you have been selcted by ${myCreator} \
-            to participate in a photography scavenger hunt at ${link}"
+	    subject '${myCreator} has invited you to their hunt, ${title}'
+	    body 'Hello, you have been selcted by ${myCreator} \
+            to participate in a photography scavenger hunt at ${link}'
 	}
     }
 
     def resendKeys(email, givenBody) {
 	mailService.sendMail {
 	    to email
-	    from "Scavengr@scavengr.com"
-	    subject "Your Hunt Keys"
+	    from 'Scavengr@scavengr.com'
+	    subject 'Your Hunt Keys'
 	    body givenBody
 	}
     }
